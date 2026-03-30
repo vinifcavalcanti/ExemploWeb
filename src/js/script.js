ea069d3aa4369aa7a748b6baa0b3b73f002c99d2
Vinicius <viniciusfuentescavalcanti@gmail.com>
2026-03-30 09:31:43 -0300

    feat: Operadores Aritméticos e Lógicos.

--- src/js/script.js
+++ src/js/script.js
@@ -44,4 +44,31 @@ console.log(exemplo7);
 
 //valor vazio
 let exemplo8 = null;
-console.log(exempl8);
+console.log(exemplo8);
+
+// OPERADORES ARITMÉTICOS
+
+const valor1 = 10;
+const valor2 = 20;
+
+console.log(valor1 + valor2);
+console.log(valor1 - valor2);
+console.log(valor1 * valor2);
+console.log(valor1 / valor2);
+
+// OPERADORES LÓGICOS
+// = atribuição
+// == comparação
+/// === valor e tipo (estriatmente iguais)
+// && = E (todas condições tem que ser verdadeiras)
+// || = OU (uma das condições tem que ser verdadeira)
+// {} - Objeto
+// [] - Array
+// () - Método
+
+console.log(valor1 < valor2);
+console.log(valor1 > valor2 && valor2 > 10); //As duas tem que ser verdadeiras
+console.log(valor1 > 100 || valor2 > 10); //Só uma precisa ser verdadeira
+console.log(valor1 == valor2);
+console.log(valor2 === valor1);
+
